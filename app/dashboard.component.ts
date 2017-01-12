bcb4ee471faf4cbfe78e2cb91104f2092ecbe9cf
West Fullerton <wfuller567@aol.com>
2017-01-11 21:32:02 -0500

    Fixed type in dashboard; app is now working.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'my-dashboard',
-    templateUrl: '<h3>My Dashboard</h3>'
+    template: '<h3>My Dashboard</h3>'
 })
 export class DashboardComponent implements OnInit {
     //constructor() { }
