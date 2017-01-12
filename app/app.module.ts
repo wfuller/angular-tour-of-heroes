7adb91c965dd97be8c24b2c58eeaee5268657119
West Fullerton <wfuller567@aol.com>
2017-01-12 15:50:39 -0500

    Starting the Http portino of the sample; this update to app.module.ts adds the Http module from Angular

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,14 +1,15 @@
 //File description: root module: tells Angular how to assemble the app
 
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
 
-import { AppComponent }  from './app.component';
-import { DashboardComponent } from './dashboard.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroService } from './hero.service';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
 
 import { AppRoutingModule }  from './app-routing.module';
 
@@ -16,6 +17,7 @@ import { AppRoutingModule }  from './app-routing.module';
   imports:      [ 
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [ 
